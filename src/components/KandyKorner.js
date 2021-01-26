54cc69566cac2d5a6449d8b07fdccb3f5872434d
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2021-01-26 14:23:21 -0600

    Added Login & Registration to application

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
 import { NavBar } from "./navigation/NavBar.js";
 import { ApplicationViews } from "./ApplicationViews.js";
+import { Login } from "./auth/Login.js";
+import { Register } from "./auth/Register.js";
 
 export const KandyKorner = () => (
   <>
-    <NavBar />
-    <ApplicationViews />
+    <Route
+      render={() => {
+        if (localStorage.getItem("kandykorner_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
   </>
 );
